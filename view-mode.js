0b0c50cd5ebcba7f2aebf8011e2dc71e47c4ee2f
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2026-07-02 14:01:41 -0300

    feat: save view mode on localStorage

--- view-mode.js
+++ view-mode.js
@@ -1,6 +1,8 @@
 const viewModeBtn = document.getElementById("view-mode");
 const body = document.querySelector("body");
 
+setViewModeBasedOnLocalStorage();
+
 function changeViewMode() {
     viewModeBtn.classList.toggle("btn-dark");
     viewModeBtn.classList.toggle("btn-light");
@@ -9,4 +11,32 @@ function changeViewMode() {
     body.classList.toggle("text-white");
     body.classList.toggle("bg-white");
     body.classList.toggle("text-black");
+
+    setViewModeOnLocalStorage();
+}
+
+function setViewModeOnLocalStorage() {
+    if (viewModeBtn.classList.contains("btn-dark")) {
+        localStorage.setItem("view-mode", "light")
+
+        return;
+    }
+        
+    localStorage.setItem("view-mode", "dark")
+}
+
+function setViewModeBasedOnLocalStorage() {
+    const viewMode = localStorage.getItem("view-mode");
+
+    if (viewMode === "dark" && !viewModeBtn.classList.contains("btn-light")) {
+        changeViewMode();
+
+        return;
+    }
+
+    if (viewMode === "light" && !viewModeBtn.classList.contains("btn-dark")) {
+        changeViewMode();
+
+        return;
+    }
 }
